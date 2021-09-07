f6e60cb300b93d6c782961509aba8dbff6e2317f
swati <skpr.rssb.fl@gmail.com>
2021-09-07 23:58:51 +0530

    issue fixes

--- example/android/app/src/main/kotlin/com/example/backgeound_location_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/backgeound_location_example/MainActivity.kt
@@ -1,13 +1,7 @@
 package com.example.backgeound_location_example
 
-import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity;
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+public class MainActivity extends FlutterActivity {
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
